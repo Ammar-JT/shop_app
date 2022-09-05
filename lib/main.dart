92592b059899315d6bb2a7e9f593a1dbfac68384
AmmarJT <AmmarTashk@gmail.com>
2022-09-05 15:43:32 +0300

    Lesson 8: with listen:false no need for delay + Using the FutureBuilder widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
-// Lesson 8: with listen:false no need for delay + improving the code + Using the FutureBuilder widget
+// Lesson 8: with listen:false no need for delay + Using the FutureBuilder widget + using FutureBuilder without request in every rebuild for widget tree
 
 // with listen:false no need for delay:
 //      - go and see it in orders_screen.dart
 //      - this will be deleted in the next git commit
 //      ..(which means it's only exist in the lesson 8 first commit)
+
+// Using the FutureBuilder widget:
+//      - see it in orders_screen.dart
+
+//
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/screens/cart_screen.dart';
